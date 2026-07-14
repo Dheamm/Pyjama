93496f231994ee0d6ada453768559e2a5bae0a6a
Ítalo Osorio <dheamhd@gmail.com>
2026-07-14 07:11:16 -0400

    add Permissions class and declare hello/reload perms in plugin.yml

---
+++ pyjama-core/src/main/java/dheam/pyjama/core/service/permission/Permissions.java
@@ -0,0 +1,10 @@
+package dheam.pyjama.core.service.permission;
+
+public final class Permissions {
+
+    public static final String COMMAND_HELLO = "pyjama.command.hello";
+    public static final String COMMAND_RELOAD = "pyjama.command.reload";
+
+    private Permissions() {
+    }
+}
